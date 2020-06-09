d8e3160acd2930fc2c397e3a36c401e161fd18b6
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-06-08 18:06:17 -0700

    Restart server upon `clean`

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,7 @@ alias runsql="sqlite3 database/sqlite3 <"
 clean () {
   rm -f database/sqlite3
   runsql create-database.sql
+  touch server.js  # To force a restart to reconnect to new DB
 }
 
 getall () {
