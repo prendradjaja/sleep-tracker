d47b0d6b145a83dd2b1e395fb4f2239dc8882e32
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-06-08 17:49:35 -0700

    Create json-only frontend

---
+++ public/app.js
@@ -0,0 +1,39 @@
+main();
+
+const displayElement = document.getElementById('display');
+const awakeButton = document.getElementById('awake-button');
+const pushingIndicator = document.getElementById('pushing-indicator');
+
+function main() {
+  refreshDisplay();
+}
+
+function displayWakeTimes(wakeTimes) {
+  displayElement.innerHTML = wakeTimes
+    .map(record => JSON.stringify(record))
+    .join('<br>');
+}
+
+function refreshDisplay() {
+  fetch('/api/wake_times')
+    .then(result => result.json())
+    .then(data => displayWakeTimes(data))
+    .catch(error => console.log('Error'));
+}
+
+function handleAwakeClick() {
+  const yes = confirm("Push wake time?");
+  if (yes) {
+    pushWakeTime();
+  }
+}
+
+function pushWakeTime() {
+  awakeButton.classList.add('hidden');
+  pushingIndicator.innerText = 'Recording wake time...';
+  fetch('/api/wake_times/now', { method: 'PUT' })
+    .then(() => {
+      // TODO error handling
+      pushingIndicator.innerText += ' Done.';
+    });
+}
