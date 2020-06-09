7343b0cbdf57cb288cd25c8a234d5c4dc5134123
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-06-08 17:32:12 -0700

    Add PUT .../now

--- server.js
+++ server.js
@@ -11,11 +11,63 @@ function startApp() {
 
   app.use(express.static('public'));
 
+  /**
+   * Get all wake times e.g.
+   *
+   * [
+   *   { date: "2020-01-01", "09:00" },
+   *   { date: "2020-01-02", "13:00" }
+   * ]
+   */
   app.get('/api/wake_times', (req, res) => {
     db.all('SELECT date, time FROM wake_times;', (err, rows) => {
       res.send(rows);
+      // TODO error handling
+    });
+  });
+
+  /**
+   * Adds or updates today's wake_times entry to the current time
+   */
+  app.put('/api/wake_times/now', (req, res) => {
+    const date = getDate();
+    const time = getTime();
+    // Don't do interpolation like this with user-provided data!
+    const command = `
+      INSERT INTO wake_times (date, time)
+      VALUES ("${date}", "${time}")
+      ON CONFLICT (date)
+      DO UPDATE SET time=excluded.time;
+    `;
+    db.run(command, (err) => {
+      res.send("done");
+      // TODO error handling
     });
   });
 
   app.listen(PORT, () => console.log('Example app listening at http://localhost:'+PORT))
+}
+
+function getDate() {
+  const today = new Date();
+  const year = (today.getFullYear())
+    .toString();
+  const month = (today.getMonth() + 1)
+    .toString()
+    .padStart(2, '0');
+  const day = (today.getDate())
+    .toString()
+    .padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
+
+function getTime() {
+  const now = new Date();
+  const hours = (now.getHours() + 1)
+    .toString()
+    .padStart(2, '0');
+  const minutes = (now.getMinutes())
+    .toString()
+    .padStart(2, '0');
+  return `${hours}:${minutes}`;
 }
